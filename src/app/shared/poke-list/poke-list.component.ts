9b8562f77b356fad0eb76e4b72802f1f4901c3cf
gholiveira29 <g.h.p.o3615@gmail.com>
2023-05-24 20:24:59 -0300

    finalizando projeto

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
+  getAllPokemons: any;
+  setAllPokemons: any;
+  apiError: boolean = false;
+  constructor(
+    private pokeApiService: PokeApiService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => {
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
+      },
+      error => {
+        this.apiError = true;
+      }
+    );
+  }
+
+  goToDetails(id: any) {
+    this.router.navigateByUrl(`details/${id}`);
+  }
+
+  getShearch(value: string) {
+    const filter = this.setAllPokemons.filter((res: any) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.getAllPokemons = filter;
+  }
 
 }
